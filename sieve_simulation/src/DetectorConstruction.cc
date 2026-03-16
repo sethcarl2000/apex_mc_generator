dbc0f401dddad9da3bc40ca56cc53532401377d6
sethcarl2000 <sethhall@jlab.org>
2026-03-16 12:10:42 -0400

    Rearange some things in DetectorConstruction routine, and prepare for correct sieve-positioning using arm geometry

--- sieve_simulation/src/DetectorConstruction.cc
+++ sieve_simulation/src/DetectorConstruction.cc
@@ -35,6 +35,9 @@
 #include "G4NistManager.hh"
 #include "G4PVPlacement.hh"
 #include "G4SystemOfUnits.hh"
+#include "CLHEP/Vector/Rotation.h"
+#include "CLHEP/Vector/RotationZ.h"
+#include "CLHEP/Vector/RotationX.h"
 #include "G4VisAttributes.hh"
 #include "G4SubtractionSolid.hh"
 #include "G4UnionSolid.hh"
@@ -100,16 +103,19 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
     checkOverlaps  // overlaps checking
   ); 
   
-  /// Position of the sieve
-  G4ThreeVector position_sieve(0., 0., 0.); 
-  /// Rotation of the sieve 
-  G4RotationMatrix *rotation_sieve = nullptr; 
-  
   /// dimensions of the sieve 
-  const G4double sieve_dx = 3.250 * inch; 
-  const G4double sieve_dy = 4.250 * inch; 
+  const G4double sieve_dx = 4.250 * inch; 
+  const G4double sieve_dy = 3.250 * inch; 
   const G4double sieve_dz = 0.500 * inch; 
 
+  /// Position of the sieve
+  G4ThreeVector position_sieve(0., 0., +sieve_dz/2.); 
+  /// Rotation of the sieve 
+  G4RotationMatrix *rotation_sieve = new G4RotationMatrix(
+    CLHEP::HepRotationX(25.*CLHEP::pi/180.) * CLHEP::HepRotationZ(CLHEP::pi/2) 
+  );
+  
+  
   /// Sieve containter volume 
   // Physical volume
   auto solid_SieveContainer = new G4Box(
@@ -181,65 +187,6 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
   ); 
   // -----------------------------------------------------
 
-/*  
-  //
-  // Shape 1
-  //
-  G4Material* shape1_mat = nist->FindOrBuildMaterial("G4_A-150_TISSUE");
-  G4ThreeVector pos1 = G4ThreeVector(0, 2 * cm, -7 * cm);
-
-  // Conical section shape
-  G4double shape1_rmina = 0. * cm, shape1_rmaxa = 2. * cm;
-  G4double shape1_rminb = 0. * cm, shape1_rmaxb = 4. * cm;
-  G4double shape1_hz = 3. * cm;
-  G4double shape1_phimin = 0. * deg, shape1_phimax = 360. * deg;
-  auto solidShape1 = new G4Cons("Shape1", shape1_rmina, shape1_rmaxa, shape1_rminb, shape1_rmaxb,
-                                shape1_hz, shape1_phimin, shape1_phimax);
-
-  auto logicShape1 = new G4LogicalVolume(solidShape1,  // its solid
-                                         shape1_mat,  // its material
-                                         "Shape1");  // its name
-
-  new G4PVPlacement(nullptr,  // no rotation
-                    pos1,  // at position
-                    logicShape1,  // its logical volume
-                    "Shape1",  // its name
-                    logicEnv,  // its mother  volume
-                    false,  // no boolean operation
-                    0,  // copy number
-                    checkOverlaps);  // overlaps checking
-
-  //
-  // Shape 2
-  //
-  G4Material* shape2_mat = nist->FindOrBuildMaterial("G4_BONE_COMPACT_ICRU");
-  G4ThreeVector pos2 = G4ThreeVector(0, -1 * cm, 7 * cm);
-
-  // Trapezoid shape
-  G4double shape2_dxa = 12 * cm, shape2_dxb = 12 * cm;
-  G4double shape2_dya = 10 * cm, shape2_dyb = 16 * cm;
-  G4double shape2_dz = 6 * cm;
-  auto solidShape2 =
-    new G4Trd("Shape2",  // its name
-              0.5 * shape2_dxa, 0.5 * shape2_dxb, 0.5 * shape2_dya, 0.5 * shape2_dyb,
-              0.5 * shape2_dz);  // its size
-
-  auto logicShape2 = new G4LogicalVolume(solidShape2,  // its solid
-                                         shape2_mat,  // its material
-                                         "Shape2");  // its name
-
-  new G4PVPlacement(nullptr,  // no rotation
-                    pos2,  // at position
-                    logicShape2,  // its logical volume
-                    "Shape2",  // its name
-                    logicEnv,  // its mother  volume
-                    false,  // no boolean operation
-                    0,  // copy number
-                    checkOverlaps);  // overlaps checking
-
-  // Set Shape2 as scoring volume
-  //
-*/ 
   fScoringVolume = logic_sieveContainer;
   //
   //  always return the physical World
