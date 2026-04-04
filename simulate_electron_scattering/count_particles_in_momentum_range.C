188fd818daf8621ee9bf1d12cf8cf9b7aca095be
sethcarl2000 <sethhall@jlab.org>
2026-04-04 14:40:53 -0400

    Change macro to also draw full energy distribution as a function of thickness

--- simulate_electron_scattering/count_particles_in_momentum_range.C
+++ simulate_electron_scattering/count_particles_in_momentum_range.C
@@ -2,6 +2,9 @@
 #include <TFile.h>
 #include <TCanvas.h>
 #include <TLegend.h>  
+#include <TH2D.h> 
+#include <TAxis.h>
+#include <TStyle.h>
 #include <math.h>
 #include <ROOT/RDataFrame.hxx>
 #include <vector> 
@@ -14,6 +17,8 @@ namespace {
     constexpr double mom_max = 1104*( 1 + 0.06 ); 
     
     constexpr double stat_scale = 4e5; 
+
+    constexpr double beam_energy = 2200; //MeV
 }
 
 double Count_particles_in_acceptance(ROOT::RDF::RNode df, double p_min, double p_max, int charge); 
@@ -23,6 +28,24 @@ int count_particles_in_momentum_range()
     const int min_thickness_um = 50; 
     const int max_thickness_um = 12500; 
     const int thickness_spacing = 50; 
+
+    const int n_pts = (max_thickness_um - min_thickness_um)/thickness_spacing; 
+
+    const int nbins_energy = 60; 
+    const double min_energy = 1000; 
+    const double max_energy = beam_energy; 
+
+    auto hist_energy_e = new TH2D(
+        "h_energye", "Energy of punch-through lepton (e-);Target Thickness (mm);Energy (MeV)", 
+        n_pts, ((double)min_thickness_um-(thickness_spacing/2))/1e3, ((double)max_thickness_um+(thickness_spacing/2))/1e3, 
+        nbins_energy, min_energy, max_energy
+    );
+    
+    auto hist_energy_p = new TH2D(
+        "h_energyp", "Energy of punch-through lepton (e+);Target Thickness (mm);Energy (MeV)", 
+        n_pts, min_thickness_um-(thickness_spacing/2), max_thickness_um+(thickness_spacing/2), 
+        nbins_energy, min_energy, max_energy
+    );
 
     vector<double> pts_thickness; 
     vector<double> pts_electron; 
@@ -36,9 +59,60 @@ int count_particles_in_momentum_range()
         cout << "Processing file '" << path_file << "'" << endl; 
         
         pts_thickness.emplace_back(((double)um)/1e3); 
-        pts_electron .emplace_back(Count_particles_in_acceptance(df, mom_min, mom_max, -1) / stat_scale ); 
-        pts_positron .emplace_back(Count_particles_in_acceptance(df, mom_min, mom_max, +1) / stat_scale );
+        //pts_electron .emplace_back(Count_particles_in_acceptance(df, mom_min, mom_max, -1) / stat_scale ); 
+        //pts_positron .emplace_back(Count_particles_in_acceptance(df, mom_min, mom_max, +1) / stat_scale );
+    
+        auto df_energy = df
+            
+            .Define("energy", [](double px, double py, double pz)
+            { 
+                return sqrt(px*px + py*py + pz*pz); 
+            }, {"momentum_sieve_x", "momentum_sieve_y", "momentum_sieve_z"});
+            
+        auto stats_e = *df_energy.Filter([](int charge, double p)
+            { return (charge==-1) && (p > mom_min) && (p < mom_max); }, {"charge", "energy"}).Count(); 
+
+        auto stats_p = *df_energy.Filter([](int charge, double p)
+            { return (charge==+1) && (p > mom_min) && (p < mom_max); }, {"charge", "energy"}).Count(); 
+
+        pts_electron.emplace_back( ((double)stats_e) / stat_scale ); 
+        pts_positron.emplace_back( ((double)stats_p) / stat_scale ); 
+
+        auto henergy_e = (TH1D*)df_energy 
+
+            .Filter([](int charge){ return charge==-1; }, {"charge"})
+
+            .Histo1D<double>({"hee", "energy", nbins_energy, min_energy, max_energy}, "energy")->Clone("hee_temp"); 
+        henergy_e->SetDirectory(0);
+        
+        auto henergy_p = (TH1D*)df_energy 
+
+            .Filter([](int charge){ return charge==+1; }, {"charge"})
+
+            .Histo1D<double>({"hpe", "energy", nbins_energy, min_energy, max_energy}, "energy")->Clone("hpe_temp"); 
+        henergy_p->SetDirectory(0);
+
+        auto xax = henergy_e->GetXaxis(); 
+        for (int bx=1; bx<=xax->GetNbins(); bx++) {
+
+           double energy = xax->GetBinCenter(bx); 
+           
+           hist_energy_e->Fill( ((double)um)/1.e3, energy, henergy_e->GetBinContent(bx) / stat_scale );
+           hist_energy_p->Fill( ((double)um)/1.e3, energy, henergy_p->GetBinContent(bx) / stat_scale ); 
+        }
+        delete henergy_e;
+        delete henergy_p;
     }
+    
+    new TCanvas; 
+    gPad->SetLogz(1);
+    gStyle->SetOptStat(0);
+    hist_energy_e->Draw("colz");
+    
+    new TCanvas; 
+    gPad->SetLogz(1);
+    hist_energy_p->Draw("colz");
+    
     auto legend = new TLegend; 
 
     new TCanvas; 
